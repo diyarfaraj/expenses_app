09bf230a593bcdbcb2d50355c01850aaac062f13
Diyar Faraj <diyar.faraj@gmail.com>
2021-11-28 21:39:25 +0100

    added viewmodels

--- ExpensesApp/Models/Expense.cs
+++ ExpensesApp/Models/Expense.cs
@@ -13,6 +13,15 @@ namespace ExpensesApp.Models
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public string Category { get; set; }
+
+        public static int InsertExpense(Expense expense)
+        {
+            using (SQLite.SQLiteConnection connection = new SQLiteConnection(App.DatebasePath))
+            {
+                connection.CreateTable<Expense>();
+                return connection.Insert(expense);
+            }
+        }
         
     }
 }
--- ExpensesApp/ViewModels/CategoriesVM.cs
+++ ExpensesApp/ViewModels/CategoriesVM.cs
@@ -1,7 +1,25 @@
-﻿namespace ExpensesApp.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace ExpensesApp.ViewModels
 {
     public class CategoriesVM
     {
-        
+        public ObservableCollection<string> Categories { get; set; }
+
+        public CategoriesVM()
+        {
+            GetCategories();
+        }
+
+        private void GetCategories()
+        {
+            Categories = new ObservableCollection<string>();
+            Categories.Add("housing");
+            Categories.Add("debt");
+            Categories.Add("health");
+            Categories.Add("personal");
+            Categories.Add("travel");
+
+        }
     }
 }
